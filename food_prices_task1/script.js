86911137f917735fcbb65dff6b61c6b9918c935b
zemlianajs <anechkaft@gmail.com>
2022-08-04 21:40:32 +0300

    change result

--- food_prices_task1/script.js
+++ food_prices_task1/script.js
@@ -13,41 +13,37 @@ let captionString = `Food prices ≠ <ВИВІД ПОТОЧНОГО ДНЯ>.<В�
     kiwi = `🥝`,
     kiwiPrice = 15,
     kiwiCount = 10,
-    kiwiCountryPercent = 10;
-
-    captionString = captionString.replace('≠', '-');
-    
-    captionString = captionString.replace('<ВИВІД ПОТОЧНОГО ДНЯ>', new Date().getDate());
+    kiwiCountryPercent = 10;;
     
     month = new Date().getMonth();
-    month++;     
-    captionString = captionString.replace('<ВИВІД ПОТОЧНОГО МІСЯЦЯ>', month);
+    month++;   
 
-    captionString = captionString.replace('<ВИВІД ПОТОЧНОГО РОКУ>',  year = new Date().getFullYear());
+    captionString = captionString.replace('≠', '-').replace('<ВИВІД ПОТОЧНОГО ДНЯ>', new Date().getDate()).replace('<ВИВІД ПОТОЧНОГО МІСЯЦЯ>', month)
+                    .replace('<ВИВІД ПОТОЧНОГО РОКУ>',  year = new Date().getFullYear());
 
-    resultStr = 'Final price for ';
-    equals = ' = ';
-    currency = ' UAH ';
-    finalStr = 'Final price for all products';
+    resultStr = `Final price for`;
+    equals = `=`;
+    currency = `UAH`;
+    finalStr = `Final price for all products`;
 
-    calculateApplePrice = Number(((applePrice - (applePrice * appleSalePercent / 100)) * appleCount).toFixed(2));
-    finalApple = resultStr + appleCount + ' ' + apple + equals + (calculateApplePrice).toFixed() + currency;
-   
-    calculateOrangePrice = Number(((orangePrice - (orangePrice * orangeSalePercent / 100)) * orangeCount).toFixed(2));
-    finalOrange = resultStr + orangeCount + ' ' + orange + equals + (calculateOrangePrice).toFixed() + currency;  
+    calculateApplePrice = +(((applePrice - (applePrice * appleSalePercent / 100)) * appleCount).toFixed(2));
+    finalApple = `${resultStr} ${appleCount} ${apple} ${equals} ${(calculateApplePrice).toFixed()} ${currency}`;
 
-    calculateKiwiPrice = Number(((kiwiPrice + (kiwiPrice * kiwiCountryPercent / 100)) * kiwiCount).toFixed(2));
-    finalKiwi = resultStr + kiwiCount + ' ' + kiwi + equals + (calculateKiwiPrice).toFixed() + currency;
+    calculateOrangePrice = +(((orangePrice - (orangePrice * orangeSalePercent / 100)) * orangeCount).toFixed(2));
+    finalOrange = `${resultStr} ${orangeCount} ${orange} ${equals} ${(calculateOrangePrice).toFixed()} ${currency}`;
+
+    calculateKiwiPrice = +(((kiwiPrice + (kiwiPrice * kiwiCountryPercent / 100)) * kiwiCount).toFixed(2));
+    finalKiwi = `${resultStr} ${kiwiCount} ${kiwi} ${equals} ${(calculateKiwiPrice).toFixed()} ${currency}`;
 
     calculateFinalPrice = calculateApplePrice + calculateOrangePrice + calculateKiwiPrice;
     allFinalPrice = finalStr + equals + calculateFinalPrice;
 
-    captionString = captionString.slice(0)
-                    + '\n' 
-                    + '\n' + finalApple 
-                    + '\n' + finalOrange 
-                    + '\n' + finalKiwi
-                    + '\n'
-                    + '\n' + allFinalPrice + currency;
+    captionString = `${captionString.slice(0)} 
+                    
+${finalApple}
+${finalOrange}
+${finalKiwi}
+
+${allFinalPrice} ${currency}`;
 
 console.log(captionString);
