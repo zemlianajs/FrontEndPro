69ceb5e7fcb39a4b44dd854f06be5508e983992c
zemlianajs <anechkaft@gmail.com>
2022-08-09 21:28:24 +0300

    bulka done

--- choose_products_task2/script.js
+++ choose_products_task2/script.js
@@ -1,42 +1,42 @@
 let value = 0;
 
-// let Bulka = prompt(`hamburger or cheeseburger`);
+let Bulka = prompt(`hamburger or cheeseburger`);
 
-// if (Bulka) Bulka = Bulka.replaceAll(` `, ``).toLowerCase();
+if (Bulka) Bulka = Bulka.replaceAll(` `, ``).toLowerCase();
 
-// if (Bulka === null || Bulka === `hamburger` || Bulka === ` ` || !Bulka.includes(`burger`) || !Bulka.startsWith(`cheese`, `hamb`)) {
-// 	Bulka = `hamburger`;
-// 	let valueHamburger = 10;
-// 	value = value + valueHamburger;
-// } else if (Bulka === `cheeseburger`){	
-// 	let cheese = confirm(`Would you like to add double cheese?`);
-// 	let valueCheeseburger = 15;
-// 	value = value + valueCheeseburger;
-// 	if (cheese) {
-// 		let valueDoubleCheese = 5;
-// 		value = value + valueDoubleCheese;
-// 	} 
-// } 
+if (Bulka === null || Bulka === `hamburger` || Bulka === ` ` || !Bulka.includes(`burger`) || !Bulka.startsWith(`cheese`, `hamb`)) {
+	Bulka = `hamburger`;
+	let valueHamburger = 10;
+	value = value + valueHamburger;
+} else if (Bulka === `cheeseburger`){	
+	let cheese = confirm(`Would you like to add double cheese?`);
+	let valueCheeseburger = 15;
+	value = value + valueCheeseburger;
+	if (cheese) {
+		let valueDoubleCheese = 5;
+		value = value + valueDoubleCheese;
+	} 
+} 
 
-let Potato = confirm(`Would you like potato?`);
+// let Potato = confirm(`Would you like potato?`);
 
-if (Potato) {
-	let sizePotato = prompt(`Choose potato size: small/middle/big`);
-	if (sizePotato) sizePotato = sizePotato.replaceAll(` `, ``).toLowerCase();
-}
-if (Potato === null || Potato === `small` || Potato === ` ` || !Potato.includes(`middle`, `big`))
-	smallPotato = 10,
-	middlePotato = 15,
-	bigePotato = 20;
+// if (Potato) {
+// 	let sizePotato = prompt(`Choose potato size: small/middle/big`);
+// 	if (sizePotato) sizePotato = sizePotato.replaceAll(` `, ``).toLowerCase();
+// }
+// if (Potato === null || Potato === `small` || Potato === ` ` || !Potato.includes(`middle`, `big`))
+// 	smallPotato = 10,
+// 	middlePotato = 15,
+// 	bigePotato = 20;
 
-if (Potato) {
-	sizePotato = prompt(`Choose potato size: small/middle/big`).replaceAll(` `, ``).toLowerCase();
+// if (Potato) {
+// 	sizePotato = prompt(`Choose potato size: small/middle/big`).replaceAll(` `, ``).toLowerCase();
 
-} 	if (sizePotato === `middle`) {
-		value = + middlePotato;
-	} else if (sizePotato === `big`) {
-		value = + bigePotato;
-	}
+// } 	if (sizePotato === `middle`) {
+// 		value = + middlePotato;
+// 	} else if (sizePotato === `big`) {
+// 		value = + bigePotato;
+// 	}
 
 // let Sauce = confirm(`Would you like sauce?`),	
 // 	ketchup = 2,
