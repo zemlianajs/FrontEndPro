950377748d55c5ec5e18619a06a79e1cb8481986
zemlianajs <anechkaft@gmail.com>
2022-08-16 15:23:54 +0300

    done

--- three_important_words _task4/script.js
+++ three_important_words _task4/script.js
@@ -1,25 +1,49 @@
-let ImportantWords;
-let words = ``;
-let countOfWords = 1;
-let typeOfTransformation;
-let userAnver
+const countOfWords = 3;
 
-ImportantWords = confirm(`Tell me three most important words 💚`);
+let words = ``,
+    typeOfTransformation,
+    sentance = ``;
+
+let ImportantWords = confirm(`Tell me three most important words 💚`);
 
 if(ImportantWords){
-    for(; countOfWords<=3; countOfWords++, words+=countOfWords){
-        words = prompt(`Enter word #${countOfWords}`);
-        if(words) words = words.replaceAll(` `, ``); 
-        console.log(`Word #${countOfWords}: ${words}`);
-        typeOfTransformation = prompt(`Choose type of transformation for "${words}" uppercase|lowercase|capitalize`, `uppercase`);
-        if(typeOfTransformation === `uppercase`) words = words.toUpperCase();
-        if(typeOfTransformation === `lowercase`) words = words.toLowerCase();
-        if(typeOfTransformation === `capitalize`) words = words[0].toUpperCase() + words.slice(1).toLowerCase();
-        console.log(`Transform type for word #${countOfWords}: ${typeOfTransformation}`)
-        console.log(`Transformed word #${countOfWords}: ${words}`)
-    }
+    for(let i=1; i<=countOfWords; i++){
+        do{
+            words = prompt(`Enter word #${i}`);
+            if(words) {
+                words = words.replaceAll(` `, ``); 
+                for(let i=0; i<words.length; i++){
+                    let letter = words[i];
+                    if(!isNaN(letter)){
+                    words = false;
+                    break;
+                    }
+                }
+            }
+        } while(!words);
+        console.log(`Word #${i}: ${words}`);
+        do{
+            typeOfTransformation = prompt(`Choose type of transformation for "${words}" uppercase|lowercase|capitalize`, `uppercase`);
+            if(typeOfTransformation) typeOfTransformation = typeOfTransformation.replaceAll(` `, ``);
+        } while (!(typeOfTransformation === `uppercase` || typeOfTransformation === `lowercase` || typeOfTransformation === `capitalize`));
+            console.log(`Transform type for word #${i}: ${typeOfTransformation}`);
+            switch(typeOfTransformation){
+            case `uppercase`:
+                words = words.toUpperCase();
+                break;
+            case `lowercase`:
+                words = words.toLowerCase();
+                break;
+            case `capitalize`:
+                words = words[0].toUpperCase() + words.slice(1).toLowerCase();
+                break
+        };
+        console.log(`Transformed word #${i}: ${words}`);
+        sentance += words;        
+        sentance += i === countOfWords ? `!` : ` `;   
+        console.log(`Sentance: ${sentance}`);
+    };
+console.log(`******`);
 }
-
-
-
+console.log(sentance);
 
