fa628961981a0b992b8e4953be208e126c4b4705
zemlianajs <anechkaft@gmail.com>
2022-08-21 18:34:12 +0300

    done

--- random_array _task5/script.js
+++ random_array _task5/script.js
@@ -1,19 +1,17 @@
 let arr = [],
     lengthOfArr,
+    minValueArr = 0,
+    maxValuetArr = 0,
     minLengthOfArr = 2,
     maxLengthOfArr = 10,
-    minValueArr,
-    maxValuetArr,
     minValueArrFrom = -10,
     minValueArrTo = 10,
     maxValuetArrTo = 50,
-    minElement,
-    maxElement,
     indexOfMin,
     indexOfMax;
 
 do{
-    lengthOfArr = +prompt(`Enter the length of array from 2 to 10`);
+    lengthOfArr = prompt(`Enter the length of array from ${minLengthOfArr} to ${maxLengthOfArr}`);
     lengthOfArr = Math.abs(lengthOfArr);
     lengthOfArr = Math.round(lengthOfArr);
 } while (!lengthOfArr || lengthOfArr<minLengthOfArr || lengthOfArr>maxLengthOfArr);
@@ -21,22 +19,28 @@ do{
 arr.length = lengthOfArr;
 
 do{
-    minValueArr = +prompt(`Enter the minimum value of random array element from -10 to 10`);
-    minValueArr = Math.round(minValueArr);
-} while (!minValueArr || minValueArr<minValueArrFrom || minValueArr>minValueArrTo)
+    minValueArr = prompt(`Enter the minimum value of random array element from ${minValueArrFrom} to ${minValueArrTo}`);
+    if(isNaN(minValueArr)) minValueArr = false;
+    if(typeof minValueArr === "number") minValueArr = Math.round(minValueArr);  
+} while (!minValueArr || minValueArr<minValueArrFrom || minValueArr>minValueArrTo);
+
+minValueArr = parseInt(minValueArr);
 
 do{
-    maxValuetArr = +prompt(`Enter the maximum value of random array element up to 50, but not less than the minimum`);
-    maxValuetArr = Math.round(maxValuetArr);
+    maxValuetArr = prompt(`Enter the maximum value of random array element up to ${maxValuetArrTo}, but not less than the ${minValueArr}`);
+    if(isNaN(maxValuetArr)) maxValuetArr = false;
+    if(typeof maxValuetArr === "number") maxValuetArr = Math.round(maxValuetArr);
 } while (!maxValuetArr || maxValuetArr<=minValueArr || maxValuetArr>maxValuetArrTo)
+
+maxValuetArr = parseInt(maxValuetArr);
 
 for(let i=0; i<arr.length; i++){
     arr[i] = Math.round(Math.random() * (maxValuetArr - minValueArr + 1) + minValueArr);
 }
 console.log(arr);
 
-minElement = arr[0];
-maxElement = minElement;
+let minElement = arr[0];
+let maxElement = minElement;
 
 for(let i=1; i<arr.length; i++){
     if(arr[i] <= minElement) minElement = arr[i];
