88045352a4f6f8aa1d82b3dbb3036a6c0dbb38b1
zemlianajs <anechkaft@gmail.com>
2022-10-18 19:57:40 +0300

    done

--- task17_shapes/script.js
+++ task17_shapes/script.js
@@ -1,2 +1,12 @@
 const element = document.querySelector(`#element`);
+const renderShape = document.querySelector(`#renderShape`);
+const shape = document.querySelector(`#shape`);
+const checkedColor = document.querySelector(`#color`);
+
+renderShape.addEventListener(`submit`, e => {
+    e.preventDefault();    
+    element.className = `${shape.value}`
+    element.style.background = checkedColor.value; 
+})
+
 
